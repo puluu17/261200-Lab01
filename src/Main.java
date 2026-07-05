1713fcf04a91a5c4368e719b49aad77b30b37168
680615525MayMyatMinHtun <maymyatminhtun.zbp@gmail.com>
2026-07-05 22:42:16 +0630

    Lab1.2 july mooln

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,42 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
+        // Initialize the turtle
+        Turtle bob = new Turtle();
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+        // 1. Set a dark midnight sky background
+        Turtle.bgcolor("gray");
+        bob.speed(0); // Set to instant drawing speed
+
+        // 2. Draw the main glowing moon base (a large yellow circle)
+        bob.up();
+        bob.setPosition(60, 60); // Position it in the upper right sky
+        bob.shape("circle");
+        bob.shapeSize(150, 150); // Make it a large full moon shape
+        bob.fillColor("gold");
+        bob.outlineColor("lightyellow");
+        bob.outlineWidth(2);
+        bob.stamp();
+
+        // 3. Create a "Crescent" effect by layering a navy circle slightly offset
+        // This acts like a shadow covering part of the moon
+        bob.setPosition(95, 60); // Shift slightly to the right
+        bob.fillColor("gray");    // Match the background color exactly
+        bob.outlineColor("gray");
+        bob.stamp();
+
+        // 4. Add a tiny twinkling star in the sky using built-in shapes
+        bob.setPosition(-100, 120);
+        bob.shape("triangle"); // Use a small triangle as a star point
+        bob.shapeSize(15, 15);
+        bob.fillColor("white");
+        bob.outlineColor("white");
+        bob.stamp();
+
+        // Flip the triangle to make it a 4-pointed star
+        bob.setDirection(270);
+        bob.stamp();
+
+        // Hide the turtle cursor when finished
+        bob.hide();
+    }
 }
